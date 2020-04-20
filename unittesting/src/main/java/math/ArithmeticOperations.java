65825a55bf3c3861d7e2eba73ac31b7e22e69b5d
iasonasma <t8160066@aueb.gr>
2020-04-20 23:38:55 +0300

    improving multiply method

--- unittesting/src/main/java/math/ArithmeticOperations.java
+++ unittesting/src/main/java/math/ArithmeticOperations.java
@@ -32,16 +32,15 @@ public class ArithmeticOperations {
 	public int multiply(int x, int y) {
 		if (x < 0 || y < 0) {
 			throw new IllegalArgumentException("x & y should be >= 0");
-		} 
-		else if (x >  Integer.MAX_VALUE/y) {
-			throw new IllegalArgumentException("The product does not fit in an Integer variable");		 
 		}
-		else if ( x == 0 || y == 0)
-		{ 
+		else if(x == 0 || y == 0) {
 			return 0;
 		}
-		return x*y;
+		 else if (x <= Integer.MAX_VALUE/y) {
+			return x*y;			
+		} else {
+			throw new IllegalArgumentException("The product does not fit in an Integer variable");		
+		}
 	}
-	
 	
 }
