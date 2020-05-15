6b93a1d3d674356ad20bf3192d0a67d4a0f421f3
iasonasma <t8160066@aueb.gr>
2020-05-16 01:01:12 +0300

    Changing DemoClient
    -DemoClient is now updated with new method coded

--- analyzermetrics/src/main/java/demo/DemoClient.java
+++ analyzermetrics/src/main/java/demo/DemoClient.java
@@ -9,35 +9,14 @@ import codeanalyzer.*;
 public class DemoClient {
 
 	public static void main(String[] args) throws IOException {
-		String filepath = "src/main/resources/TestClass.java";
-		String sourceCodeAnalyzerType = "regex";
-		String sourceFileLocation = "local";
-		String outputFilePath = "output_metrics";
-		String outputFileType = "csv";
-		
-		if(args.length == 5) {
-			filepath = args[0];
-			sourceCodeAnalyzerType = args[1];
-			sourceFileLocation = args[2];
-			outputFilePath = args[3];
-			outputFileType = args[4];
-		} else if (args.length != 0) {
+
+		if(args.length < 5) {
 			System.out.println("Incorrect number of arguments.");
 			System.exit(1);
 		}
-
-		SourceCodeAnalyzer analyzer = new SourceCodeAnalyzer(sourceFileLocation);
-		int loc = analyzer.calculateLOC(filepath, sourceCodeAnalyzerType);
-		int nom = analyzer.calculateNOM(filepath, sourceCodeAnalyzerType);
-		int noc = analyzer.calculateNOC(filepath, sourceCodeAnalyzerType);
-		
-		Map<String, Integer> metrics = new HashMap<>();
-		metrics.put("loc",loc);
-		metrics.put("nom",nom);
-		metrics.put("noc",noc);
 				
-		MetricsExporter exporter = new MetricsExporter();
-		exporter.writeFile(outputFileType, metrics, outputFilePath);
+		MetricsAnalyzer exporter = new MetricsAnalyzer();
+		exporter.calculateMetrics(args[0],args[1],args[2],args[3],args[4]);
 	}
 
 }
