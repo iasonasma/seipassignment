e4cb59ae390448dce754b31406ea7a55151ed8f3
iasonasma <t8160066@aueb.gr>
2020-05-19 02:09:19 +0300

    Added new testcase for CsvParser
    -Test case will check if file has been made

---
+++ analyzermetrics/src/test/java/analyzermetrics/CsvParserTest.java
@@ -0,0 +1,51 @@
+package analyzermetrics;
+
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import codeanalyzer.CsvParser;
+
+
+public class CsvParserTest {
+	
+	CsvParser mex = new CsvParser();
+	
+	static List<String> listofstrings = new ArrayList<String>();
+	private static   String TEST_CLASS_LOCAL = "src/test/resources/TestClass.java";
+	private static String OutputFilePath = "src/test/resources/output_metrics";
+			
+	@Before
+	public  void setUp() throws IOException {
+		listofstrings.add("loc,noc,nom");
+		listofstrings.add("7,3,3");
+	}
+	
+	@Test
+	public void testWriteCsv() {
+
+		// generate and write the output file
+		mex.writeToFile(listofstrings, OutputFilePath);
+		
+		// evaluate that the file exists
+		File outputFile = new File(OutputFilePath + ".csv");
+		Assert.assertTrue(outputFile.exists());
+		
+		// delete the generated file
+		outputFile.delete();
+		
+	}
+	
+
+	
+}
